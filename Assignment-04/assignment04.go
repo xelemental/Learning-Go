fa995d810cc080d2c66c3dbc2dfbb3b35ae1ded3
Elemental X <49472311+xelemental@users.noreply.github.com>
2021-12-07 19:03:51 +0530

    Create assignment04.go

---
+++ Assignment-04/assignment04.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 5 + 5i
+	var b = 9 + 4i
+
+	var result1 = a + b
+	var result2 = a - b
+	var result33 = a * b
+	var result4 = a / b
+
+	fmt.Println(result1, result2, result3, result4)
+}
