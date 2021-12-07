d2e546ccd0f2301e74a07ac7d62ba7b1b8d7cca1
Elemental X <49472311+xelemental@users.noreply.github.com>
2021-12-07 13:39:44 +0530

    Create assignment2.go

---
+++ Assignment-02/assignment2.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a int = 69
+	var b float64 = 78.094
+	var c byte = 'E'
+	var d bool = false
+
+	fmt.Printf("%d is an integer\n", a)
+	fmt.Printf("%f is a float\n", b)
+	fmt.Printf("%c is a character\n", c)
+	fmt.Printf("%t is a boolean\n", d)
+
+}
